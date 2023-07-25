41ad5af6f4dd18fd5b1cf3dd3510c87adafacd12
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-07-25 16:43:12 -0300

    Add f() playRound

--- script.js
+++ script.js
@@ -25,3 +25,22 @@ scissors.addEventListener("click", function () {
 });
 
 /* Function that represents the functioning of the game */
+
+const playRound = () => {
+  const choices = ["rock", "paper", "scissers"];
+  computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+  if (playerChoice === computerChoice) {
+    return "It's a tie";
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("You win this round!");
+    playerScore++;
+  } else {
+    console.log("You lose this round!");
+    computerScore++;
+  }
+};
