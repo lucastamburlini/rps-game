ccbe0eff89a02a41d59d6764bba580e5c7e46f0d
Lucas Gabriel Tamburlini <74024761+lucastamburlini@users.noreply.github.com>
2023-07-26 10:31:26 -0300

    Add name and button

--- script.js
+++ script.js
@@ -2,15 +2,41 @@ const rockBtn = document.getElementById("rock");
 const paperBtn = document.getElementById("papel");
 const scissorsBtn = document.getElementById("scissors");
 
-let containerUserScore = document.getElementById("userScore");
-let containerComputerScore = document.getElementById("computerScore");
+const startButton = document.getElementById("startButton");
+const gameSetup = document.getElementById("gameSetup");
+const gameSection = document.getElementById("gameSection");
+
+const instructions = document.getElementById("instructionsGame");
+const containerUserScore = document.getElementById("userScore");
+const containerComputerScore = document.getElementById("computerScore");
 
 let userScore = 0;
 let computerScore = 0;
 
-rockBtn.addEventListener("click", playGame);
-paperBtn.addEventListener("click", playGame);
-scissorsBtn.addEventListener("click", playGame);
+startButton.addEventListener("click", startGame);
+
+function startGame() {
+  const playerName = document.getElementById("playerName").value;
+
+  if (playerName.trim() === "") {
+    alert("Please enter your name.");
+    return;
+  }
+
+  // Hide the game setup and display the game section
+  gameSetup.style.display = "none";
+  gameSection.style.display = "block";
+
+  // You can do additional setup tasks here if needed.
+
+  // Now that the game has started, add event listeners to the buttons
+  rockBtn.addEventListener("click", playGame);
+  paperBtn.addEventListener("click", playGame);
+  scissorsBtn.addEventListener("click", playGame);
+
+  const namePlayer = document.getElementById("containerUser");
+  namePlayer.innerHTML = playerName;
+}
 
 function playGame(e) {
   let choice = ["rock", "paper", "scissors"];
@@ -32,10 +58,10 @@ function playGame(e) {
 
 winUser = () => {
   userScore++;
-  console.log("User: " + userScore);
+  containerUserScore.innerHTML = userScore;
 };
 
 winComputer = () => {
   computerScore++;
-  console.log("Computer: " + computerScore);
+  containerComputerScore.innerHTML = computerScore;
 };
